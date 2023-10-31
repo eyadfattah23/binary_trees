4fadd7f6215707abe4cc60b960b5d55554c9682c
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-10-31 15:41:33 +0200

    comment for later

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 	if (1 + binary_tree_height(tree->left) >= binary_tree_height(tree->right) + 1)
 	{
-		/* code */
+		/*reverse the if statments*/
 		if (tree->left)
 		{
 			return (1 + binary_tree_height(tree->left));
