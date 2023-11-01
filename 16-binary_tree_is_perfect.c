6dd9f397e52ad18173701a41909a686dd3ada500
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-11-01 12:59:24 +0200

    use mathematics instead

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,25 +1,37 @@
 #include "binary_trees.h"
 #include "9-binary_tree_height.c"
-#include "15-binary_tree_is_full.c"
-#include "14-binary_tree_balance.c"
+#include "11-binary_tree_size.c"
+
+/**
+ * power - calculates x ^ y
+ * @x: base
+ * @y: exponent
+ * Return: x to the power of y
+ */
+size_t power(size_t x, size_t y)
+{
+	size_t i, result = 1;
+
+	for (i = 0; i < y; i++)
+	{
+		result *= x;
+	}
+	return (result);
+}
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect or not
  * @tree: pointer to the root node of the tree to check
  * Return: If tree is NULL returns 0, otherwise
  * if the size = 2^height - 1 returns 1
- * of if left-subtree_height = right-subtree_height
- * and all nodes are full and it's balance factor is 0
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t lheight = binary_tree_height(tree->left);
-	size_t rheight = binary_tree_height(tree->right);
-	size_t height_check = (lheight == rheight);
-	int full_check = binary_tree_is_full(tree);
+	size_t height = binary_tree_height(tree), size = binary_tree_size(tree);
 
 	if (!tree)
-	{
 		return (0);
-	}
-	return (height_check && full_check && (!binary_tree_balance(tree)));
+
+	if (size == power(2, height + 1) - 1)
+		return (1);
+	return (0);
 }
